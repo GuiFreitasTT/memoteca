edbc5d9c266eb25e68c173109527a71ab51c2d04
GuiFreitasTT <guidiegofreitas@yahoo.com.br>
2022-12-01 15:05:25 -0300

    -m up

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class PensamentoService {
 
-  private readonly API = ' http://localhost:3000/pensamentos'
+  private readonly API = 'http://localhost:3000/pensamentos'
 
   constructor(private http: HttpClient) { }
 
